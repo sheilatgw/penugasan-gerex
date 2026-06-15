8b7c12ead947320ff8ac5e8f8346fecf6c8e0239
Sheila Alvina Tsabita <sheilaalvinat@gmail.com>
2026-06-15 17:04:17 +0700

    Update facultyTheme header colors to hex values

--- src/lib/faculty.ts
+++ src/lib/faculty.ts
@@ -1,41 +1,41 @@
 export const facultyTheme = {
   fteic: {
-    header: 'bg-yellow-400',
+    header: 'bg-[#FFD700]',
     name: 'Fakultas Teknologi Elektro dan Informatika Cerdas',
     mobileName: 'FTEIC',
   },
   ftirs: {
-    header: 'bg-red-700',
+    header: 'bg-[#b31e23]',
     name: 'Fakultas Teknologi Industri dan Rekayasa Sistem',
     mobileName: 'FTIRS',
   },
   fdkbd: {
-    header: 'bg-purple-700',
+    header: 'bg-[#4b0082]',
     name: 'Fakultas Desain Kreatif dan Bisnis Digital',
     mobileName: 'FDKBD',
   },
   fsad: {
-    header: 'bg-green-600',
+    header: 'bg-[#0F8140]',
     name: 'Fakultas Sains dan Analitika Data',
     mobileName: 'FSAD',
   },
   ftspk: {
-    header: 'bg-stone-950',
+    header: 'bg-[#231F20]',
     name: 'Fakultas Teknik Sipil Perencanaan dan Kebumian',
     mobileName: 'FTSPK',
   },
   ftk: {
-    header: 'bg-blue-700',
+    header: 'bg-[#26AEE4]',
     name: 'Fakultas Teknologi Kelautan',
     mobileName: 'FTK',
   },
   fv: {
-    header: 'bg-red-500',
+    header: 'bg-[#F47D52]',
     name: 'Fakultas Vokasi',
     mobileName: 'FV',
   },
   fkk: {
-    header: 'bg-gray-400',
+    header: 'bg-[#12b0a2]',
     name: 'Fakultas Kedokteran dan Kesehatan',
     mobileName: 'FKK',
   },
